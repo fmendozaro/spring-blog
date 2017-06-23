001142337f90b2b272e9ec1657737f66306dec8f
Fernando <fernandoza@gmail.com>
2017-06-23 11:50:21 -0500

    - Fixed the footer and updated the version of Materialize CSS. - Fixed the Footer margin on the body.

--- seeders.sql
+++ seeders.sql
@@ -1,8 +1,9 @@
+# Roles
 insert into roles (role) VALUES("ROLE_ADMIN");
 insert into roles (role) VALUES("ROLE_USER");
 insert into roles (role) VALUES("ROLE_GUEST");
 
-#tags
+# Tags
 insert into tags (name) VALUES ("general");
 insert into tags (name) VALUES ("tech");
 insert into tags (name) VALUES ("music");
