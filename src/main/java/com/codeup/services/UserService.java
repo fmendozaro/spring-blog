c8b02fbd203af0b58c0d093b2e6ccbbfe5ae3283
Fernando <fernandoza@gmail.com>
2017-11-16 11:47:53 -0600

    Refactor and add the authenticate method.

--- src/main/java/com/codeup/services/UserService.java
+++ src/main/java/com/codeup/services/UserService.java
@@ -2,14 +2,20 @@ package com.codeup.services;
 
 import com.codeup.models.User;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 /**
  * Created by Fer on 1/11/17.
  */
 @Service("usersSvc")
-public class UserSvc {
+public class UserService {
 
     public boolean isLoggedIn() {
         boolean isAnonymousUser =
@@ -26,6 +32,18 @@ public class UserSvc {
 
     public boolean isOwner(User postUser){
         return (postUser.getUsername().equals(loggedInUser().getUsername()));
+    }
+
+    public void authenticate(User user) {
+        // Notice how we're using an empty list for the roles
+        UserDetails userDetails = new UserWithRoles(user, Collections.emptyList());
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                userDetails.getPassword(),
+                userDetails.getAuthorities()
+        );
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(auth);
     }
 
 }
