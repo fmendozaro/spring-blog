5f50117b273ed27175b25dc88c42bc45e850ea47
Fernando <fernandoza@gmail.com>
2017-11-20 14:36:35 -0600

    Change the service to a component annotation

---
+++ src/main/java/com/codeup/services/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.codeup.services;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
+@Component
+public class ScheduledTasks {
+//    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+//
+//    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+//
+//    @Scheduled(fixedRate = 5000)
+//    public void reportCurrentTime() {
+//        log.info("The time is now {}", dateFormat.format(new Date()));
+//    }
+}
