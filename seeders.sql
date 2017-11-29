c448fb9e816fae3716c13c92b3398d70131ede53
Fernando <fernandoza@gmail.com>
2017-11-29 11:10:12 -0600

    Delete manual seeder, i use the ones built in.

--- seeders.sql
+++
@@ -1,8 +0,0 @@
-USE blog_db;
-
-# Tags
-insert into tags (name) VALUES ("General");
-insert into tags (name) VALUES ("Tech");
-insert into tags (name) VALUES ("Music");
-insert into tags (name) VALUES ("Art");
-insert into tags (name) VALUES ("Programming");
