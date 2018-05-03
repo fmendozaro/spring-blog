5a3b387c8a63346efb265a08f1ed973a6c1e2e81
fer <fernandoza@gmail.com>
2018-05-03 11:52:34 -0500

    feat: add the event class model obj

--- src/main/java/com/codeup/models/Event.java
+++ src/main/java/com/codeup/models/Event.java
@@ -1,16 +1,41 @@
 package com.codeup.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "events")
 public class Event {
 
-    private String title;
-    private String start;
+    @Id
+    @GeneratedValue
+    private long id;
 
-    public Event(String title, String start) {
+    @Column
+    private String title;
+
+    @Column
+    private Date start_date;
+
+    @Column
+    private Date end_date;
+
+    public Event(String title, Date start_date, Date end_date) {
         this.title = title;
-        this.start = start;
+        this.start_date = start_date;
+        this.end_date = end_date;
     }
 
     public Event() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -21,11 +46,19 @@ public class Event {
         this.title = title;
     }
 
-    public String getStart() {
-        return start;
+    public Date getStart_date() {
+        return start_date;
     }
 
-    public void setStart(String start) {
-        this.start = start;
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
     }
 }
