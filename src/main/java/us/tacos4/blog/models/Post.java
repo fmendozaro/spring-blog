123cb1a3e54ec646ce431cd5d0ca80df6737f580
fer <fernandoza@gmail.com>
2018-10-18 10:42:29 -0500

    fix: ids should be IDENTITY not Auto

--- src/main/java/us/tacos4/blog/models/Post.java
+++ src/main/java/us/tacos4/blog/models/Post.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Title can't be empty.")
--- src/main/java/us/tacos4/blog/models/UserRole.java
+++ src/main/java/us/tacos4/blog/models/UserRole.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class UserRole {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "user_id")
