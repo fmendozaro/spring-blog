84bac6c0cf92277dd431c738978879bb2e2955e6
Fer <fernandoza@gmail.com>
2019-10-06 19:23:33 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/codeup/BlogApp.java
    #	src/main/java/com/codeup/Lectures.java
    #	src/main/java/com/codeup/controllers/GreetingController.java
    #	src/main/java/com/codeup/controllers/HomeController.java
    #	src/main/java/com/codeup/controllers/MathController.java
    #	src/main/java/com/codeup/controllers/PostsController.java
    #	src/main/java/com/codeup/models/Post.java
    #	src/main/java/com/codeup/models/Tag.java
    #	src/main/java/com/codeup/models/User.java
    #	src/main/java/com/codeup/models/UserRole.java
    #	src/main/java/com/codeup/repositories/Posts.java
    #	src/main/java/com/codeup/repositories/UserRoles.java
    #	src/main/java/com/codeup/repositories/Users.java
    #	src/main/java/com/codeup/services/SecurityConfiguration.java
    #	src/main/java/com/codeup/services/UserDetailsLoader.java
    #	src/main/java/com/codeup/services/UserWithRoles.java
    #	src/main/java/com/fer-mendoza/blog/BlogApp.java
    #	src/main/java/com/fer-mendoza/blog/Lectures.java
    #	src/main/java/com/fer-mendoza/blog/SecurityConfiguration.java
    #	src/main/java/com/fer-mendoza/blog/controllers/GreetingController.java
    #	src/main/java/com/fer-mendoza/blog/controllers/HomeController.java
    #	src/main/java/com/fer-mendoza/blog/controllers/MathController.java
    #	src/main/java/com/fer-mendoza/blog/controllers/PostsController.java
    #	src/main/java/com/fer-mendoza/blog/models/Post.java
    #	src/main/java/com/fer-mendoza/blog/models/Tag.java
    #	src/main/java/com/fer-mendoza/blog/models/User.java
    #	src/main/java/com/fer-mendoza/blog/models/UserRole.java
    #	src/main/java/com/fer-mendoza/blog/repositories/PostRepository.java
    #	src/main/java/com/fer-mendoza/blog/repositories/UserRoles.java
    #	src/main/java/com/fer-mendoza/blog/repositories/UsersRepository.java
    #	src/main/java/com/fer-mendoza/blog/services/UserDetailsLoader.java
    #	src/main/java/com/fer-mendoza/blog/services/UserWithRoles.java
    #	src/main/java/us/tacos4/blog/BlogApp.java
    #	src/main/java/us/tacos4/blog/Lectures.java
    #	src/main/java/us/tacos4/blog/SecurityConfiguration.java
    #	src/main/java/us/tacos4/blog/controllers/GreetingController.java
    #	src/main/java/us/tacos4/blog/controllers/HomeController.java
    #	src/main/java/us/tacos4/blog/controllers/MathController.java
    #	src/main/java/us/tacos4/blog/controllers/PostsController.java
    #	src/main/java/us/tacos4/blog/models/Post.java
    #	src/main/java/us/tacos4/blog/models/Tag.java
    #	src/main/java/us/tacos4/blog/models/User.java
    #	src/main/java/us/tacos4/blog/models/UserRole.java
    #	src/main/java/us/tacos4/blog/repositories/PostRepository.java
    #	src/main/java/us/tacos4/blog/repositories/UserRoles.java
    #	src/main/java/us/tacos4/blog/repositories/UsersRepository.java
    #	src/main/java/us/tacos4/blog/services/UserDetailsLoader.java
    #	src/main/java/us/tacos4/blog/services/UserWithRoles.java
    #	src/main/resources/application.properties.example
    #	src/main/resources/templates/demos/map.html

---
+++ src/test/UserIntegrationTests.java
@@ -0,0 +1,40 @@
+import com.fer_mendoza.blog.controllers.HomeController;
+import com.fer_mendoza.blog.repositories.UsersRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class UserIntegrationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UsersRepository usersDao;
+
+    @Test
+    public void createAUserAndReturnToIndex() throws Exception {
+        this.mvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
