ba057d8a72de3af72cceca7d64dc0343bd69ba2e
Fer <fernandoza@gmail.com>
2019-10-16 15:11:38 -0500

    feat: add enough structure for first integration tests

--- src/test/UserIntegrationTests.java
+++ src/test/UserIntegrationTests.java
@@ -1,40 +1,60 @@
-import com.fer_mendoza.blog.controllers.HomeController;
+import com.fer_mendoza.blog.BlogApp;
 import com.fer_mendoza.blog.repositories.UsersRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(classes = BlogApp.class)
 @AutoConfigureMockMvc
 public class UserIntegrationTests {
 
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private UsersRepository usersDao;
+    // Sanity Test, just to make sure the mvc bean is working
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(mvc).isNotNull();
+    }
 
     @Test
-    public void createAUserAndReturnToIndex() throws Exception {
-        this.mvc.perform(post("/users/create")
-                .contentType(MediaType.APPLICATION_JSON))
+    public void testGetHomePage() throws Exception {
+        this.mvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void testRegisterUser() throws Exception {
+        this.mvc.perform(post("/register")
+                .param("username", "stacy")
+                .param("email", "stacy@email.com")
+                .param("password", "kitty")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"));
+    }
+
+//    @Test
+//    public void testRegisterUserMismatchedPasswords() throws Exception{
+//        this.mvc.perform(post("/register")
+//                .param("username", "stacy")
+//                .param("email", "stacy@email.com")
+//                .param("password", "kitty")
+//                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+//                .andExpect(status().isFound())
+//                .andExpect(redirectedUrl("/register?error"));
+//    }
 
 }
